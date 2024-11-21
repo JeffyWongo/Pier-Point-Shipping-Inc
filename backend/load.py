ad08c340e53e2db9126b0afa161ea61d84fd3f66
Andy Chen <andrewr.chen@gmail.com>
2024-11-20 22:47:01 -0800

    added load.py

---
+++ backend/load.py
@@ -0,0 +1,16 @@
+"""
+Purpose of class Load:
+    1. Run load / unload operation
+"""
+
+import ship
+
+class Load:
+    @staticmethod
+    def run():
+        ship_layout = ship.Ship()
+        print("empty for now")
+    
+
+# testing
+Load.run()
