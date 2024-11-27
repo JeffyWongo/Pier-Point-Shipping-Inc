111f85faecdcd8cc14eefebbae2bdaea57441285
Andy Chen <andrewr.chen@gmail.com>
2024-11-26 17:16:55 -0800

    unload h counts containers on top

--- backend/load.py
+++ backend/load.py
@@ -16,36 +16,34 @@ class Load:
     # function called by main program
     @staticmethod
     def run(ship_layout, unload_list, load_list):
-        # TODO: how we getting containers (or location) to unload and locations to load to
         Load.a_star(ship_layout, unload_list, load_list)
 
     
     # a star algorithm for moves to do
     @staticmethod
-    def a_star(ship_layout, unload_list, load_list):
+    def a_star(ship_layout, full_unload_list, full_load_list):
         # ASSUMPTION: unload_list and load_list stores a pair (container (name needed) and its location)
 
         # initialize frontier, explored, and solution
         frontier = queue.PriorityQueue()
         explored = {}
-        solution_map = {} # TODO: keep track of previous states
+        solution_map = {}
         
         # pair stores (f, g, h, state)
         # f = g (cost) + h (heuristic)
-        initial_h = Load.calc_heuristic(ship_layout, unload_list, load_list)
-        frontier.put((initial_h, 0, initial_h, ship_layout.copy()))
+        initial_h = Load.calc_heuristic(ship_layout, full_unload_list, full_load_list)
+        frontier.put((initial_h, 0, initial_h, ship_layout.copy(), full_unload_list, full_load_list)) # TODO: need to update unload and load list as we go (put in the tuple)
 
         # loops for each state in queue
         while not frontier.empty():
-            _, current_cost, current_h, current_layout = frontier.get()
+            _, current_cost, current_h, current_layout, unload_list, load_list = frontier.get()
             hashable_layout = tuple(tuple(row) for row in current_layout)
 
-            # TODO: check goal state
+            # check goal state
             if(Load.check_goal_state(current_layout, unload_list, load_list)):
                 return Load.reconstruct_path(solution_map, current_layout)
 
             # check if state is already explored
-            # TODO: don't load these states into frontier (when frontier implemented)
             if(explored[hashable_layout]==True):
                 continue
 
@@ -53,7 +51,10 @@ class Load:
             explored[hashable_layout] = True
 
             # TODO: add all possible moves to the frontier
-            frontier.put(current.first, Load.calc_heuristic(ship_layout, unload_list, load_list), [])
+            # TODO: don't load these states into frontier (when frontier implemented)
+            # TODO: update load and unload list for each state
+            # TODO: keep track of previous states
+            frontier.put()
 
 
     # reconstruct path when solution is found
@@ -63,6 +64,7 @@ class Load:
         return
 
     # check if goal state is satisfied
+    # TODO: works fine now. but can be simpler after frontier is finished
     @staticmethod
     def check_goal_state(ship_layout, unload_list, load_list):
         return Load.check_unload_goal(ship_layout, unload_list) & Load.check_load_goal(ship_layout, load_list)
@@ -103,26 +105,34 @@ class Load:
     @staticmethod
     def calc_unload_h(ship_layout, unload_list):
         sum = 0
-        for location in unload_list:
-            # TODO: check if container is off
-            # function param: (x, y)
-            sum += Load.load_unload_heuristic(0, 0) # TODO: add number of containers on top
+        for _, location in unload_list:
+            r, c = location
+            # TODO: for each column, check what lowest container is (currenly, if 2 containers same col, we overcount)
+            # Check if there are containers on top (add to h)
+            for row in range(r + 1, 8):  # Iterate above the current position
+                if ship_layout[row][c].name != "NaN":  # Check if empty
+                    sum += 1
+                else:
+                    break
+            # distance to (9,1)
+            sum += Load.load_unload_heuristic(r, c)
         return sum
 
 
     # heuristic for an individual container to unload
     @staticmethod
     def load_unload_heuristic(x: int, y: int):
-        return abs(7 - x) + (y)
+        return abs(8 - x) + y
 
 
     # part of heuristic for loading
     @staticmethod
     def calc_load_h(load_list):
         sum = 0
-        for location in load_list:
-            # function param: (x, y)
-            sum += Load.load_unload_heuristic(0, 0)
+        for _, location in load_list:
+            # distance to (9,1)
+            r, c = location
+            sum += Load.load_unload_heuristic(r, c)
         return sum
 
 
@@ -144,6 +154,9 @@ load_container = Container("B", 200)
 
 # 8 x 12
 layout = [[Container() for i in range(0,12)] for j in range(0,8)]
-layout[1][3] = Container("A", 120)
+layout[0][0] = Container("A", 120)
+layout[1][0] = Container("C", 400)
 
-Load.run(layout, [(unload_container, (1, 3))], [(load_container, (1, 4))])
+# Load.run(layout, [(unload_container, (1, 3))], [(load_container, (1, 4))])
+h = Load.calc_heuristic(layout, [(unload_container, (0, 0))], [(load_container, (0, 1))])
+print(h)
