8c9ae52368632723ec67fa68f65af7b18f8e674b
DanielYao777 <117966281+DanielYao777@users.noreply.github.com>
2024-12-06 19:19:21 +0000

    solved bug: if there's object above the target container

--- balance/Balance-main.cpp
+++ balance/Balance-main.cpp
@@ -6,47 +6,107 @@
 using namespace std;
 
 int main() {
+
     vector<vector<int>> ship(8, vector<int>(12, 0));
-    ship[3][2] = 10;  // element on the left to test
-    ship[3][4] = 10;  // test case to block the route
-    ship[2][5] = 10;  // test case to block the route
-    ship[5][10] = 5;  // element on the right to test
-    ship[5][7] = 15;  // test case to block the route
 
     Balance balance(ship);
 
+    // test data
+    /* //this is the bug case - not yet solved
+    balance.modifyShip(7, 2, 10);
+    balance.modifyShip(7, 7, 15);
+    balance.modifyShip(7, 5, 20);
+    balance.modifyShip(7, 6, 5);
     balance.printShip();
+    */
 
-    //test from left to right
-    int testX = 3;
-    int testY = 2;
-    cout << "\ntest moving block [" << testX << "][" << testY << "] from left to right：" << endl;
-    auto path1 = balance.findShortestPath(3, 2); // test [3][2]
-    if (!path1.empty()) {
-        cout << "successful! here's the route：" << endl;
-        for (size_t i = 0; i < path1.size(); ++i) {
-            cout << "Step " << i + 1 << ": (" << path1[i].first << ", " << path1[i].second << ")" << endl;
-        }
-    }
-    else {
-        cout << "failed, can't find a route" << endl;
-    }
+    /*
+    //test case1
+    balance.modifyShip(7, 0, -1);
+    balance.modifyShip(7, 1, -1);
+    balance.modifyShip(7, 2, -1);
+    balance.modifyShip(7, 9, -1);
+    balance.modifyShip(7, 10, -1);
+    balance.modifyShip(7, 11, -1);
+    balance.modifyShip(6, 11, -1);
+    balance.modifyShip(7, 1, 99);
+    balance.modifyShip(7, 2, 100);
+    */
+
+    /*
+    //test case2
+    balance.modifyShip(7, 3, 120);
+    balance.modifyShip(7, 8, 35);
+    balance.modifyShip(6, 2, 50);
+    balance.modifyShip(5, 0, 40);
+    */
+
+    /*
+    //test case3
+    balance.modifyShip(7, 0, 10001);
+    balance.modifyShip(7, 1, 500);
+    balance.modifyShip(7, 2, 600);
+    balance.modifyShip(7, 3, 100);
+    balance.modifyShip(6, 0, 9041);
+    balance.modifyShip(6, 1, 10);
+    */
+
+    /*
+    //test case4
+    balance.modifyShip(7, 0, -1);
+    balance.modifyShip(7, 1, -1);
+    balance.modifyShip(7, 2, -1);
+    balance.modifyShip(7, 3, -1);
+    balance.modifyShip(7, 4, -1);
+    balance.modifyShip(7, 5, -1);
+    balance.modifyShip(7, 6, -1);
+    balance.modifyShip(7, 7, -1);
+    balance.modifyShip(7, 8, -1);
+    balance.modifyShip(7, 9, -1);
+    balance.modifyShip(7, 10, -1);
+    balance.modifyShip(7, 11, -1);
+    balance.modifyShip(6, 4, 2000);
+    balance.modifyShip(6, 11, -1);
+    balance.modifyShip(5, 4, 2007);
+    balance.modifyShip(4, 4, 2011);
+    balance.modifyShip(3, 4, 10000);
+    balance.modifyShip(2, 4, 2020);
+    balance.modifyShip(1, 4, 1100);
+    balance.modifyShip(0, 4, 3044);
+    */
+
+    /*
+    //test case5
+    balance.modifyShip(7, 0, -1);
+    balance.modifyShip(7, 1, 96);
+    balance.modifyShip(7, 2, 8);
+    balance.modifyShip(7, 3, 4);
+    balance.modifyShip(7, 4, 4);
+    balance.modifyShip(7, 5, 1);
+    balance.modifyShip(7, 11, -1);
+    */
+
+    
+    //test case SilverQueen
+    balance.modifyShip(7, 0, -1);
+    balance.modifyShip(7, 1, 60);
+    balance.modifyShip(7, 2, 20);
+    balance.modifyShip(7, 3, 20);
+    balance.modifyShip(7, 11, -1);
+    balance.modifyShip(6, 1, 20);
+    
 
 
-    //test from right to left
-    testX = 5;
-    testY = 10;
-    cout << "\ntest moving block [" << testX << "][" << testY << "] from right to left：" << endl;
-    auto path2 = balance.findShortestPath(5, 10); // test [5][10]
-    if (!path2.empty()) {
-        cout << "successful, here's the route：" << endl;
-        for (size_t i = 0; i < path2.size(); ++i) {
-            cout << "Step " << i + 1 << ": (" << path2[i].first << ", " << path2[i].second << ")" << endl;
-        }
+    balance.printShip();
+    while (!balance.isBalanced()) {
+        balance.calculateSums();
+        balance.printShipWeight();
+        balance.printBestMove();
+        balance.printShip();
+        balance.printShipWeight();
     }
-    else {
-        cout << "failed, can't find a route" << endl;
-    }
+
+    cout << "Congrats! it's balanced now." << endl;
 
     return 0;
 }
--- balance/Balance.cpp
+++ balance/Balance.cpp
@@ -4,77 +4,314 @@
 #include <queue>
 #include <unordered_map>
 #include <cmath>
+#include <iomanip>
 #include <string>
 
 using namespace std;
 
 Balance::Balance(vector<vector<int>>& initialShip) : ship(initialShip) {}
 
-int Balance::calculateHeuristic(int row, int col, bool moveToRight) { // doesn't really need row tho, but keep it to keep everything clear
-    if (moveToRight) {
-        return abs(col - 6); // how long still need to get to the right
-    }
-    else {
-        return abs(col - 5); // how long still need to get to the left
-    }
+int Balance::calculateHeuristic(int row, int col, int targetRow, int targetCol) {
+    return abs(row - targetRow) + abs(col - targetCol); // 曼哈頓距離
 }
 
+
 vector<pair<int, int>> Balance::findShortestPath(int startRow, int startCol) {
-    priority_queue<Node, vector<Node>, greater<Node>> openList; // use this so smallest h() value is at the very front
+    priority_queue<Node, vector<Node>, greater<Node>> openList;
     unordered_map<string, bool> visited;
-    vector<pair<int, int>> directions = { {-1, 0}, {1, 0}, {0, -1}, {0, 1} }; // possible directions
+    vector<pair<int, int>> directions = { {-1, 0}, {1, 0}, {0, -1}, {0, 1} };
 
-    // determine if the element if moving to left to moving to right
-    bool moveToRight = (startCol < 6); // if less than 6, then it's on the left so moving to right
+    // if target on the left then move to right, if on the right then move to left
+    bool moveToRight = (startCol < cols / 2);
 
-    // push the startNode to the openList
-    Node startNode = { startRow, startCol, 0, calculateHeuristic(startRow, startCol, moveToRight), {} }; //row, col, g, h, path
+    int targetCol = moveToRight ? cols / 2 : cols / 2 - 1;
+    int targetRow = -1;
+
+    if (moveToRight) {
+        // if on the left then move to right
+        while (targetCol >= cols / 2 && targetCol < cols) {
+            for (int r = rows - 1; r >= 0; --r) { // start at the bottom
+                if (ship[r][targetCol] == 0) {
+                    if (r == rows - 1 || ship[r + 1][targetCol] != 0) {
+                        targetRow = r;
+                        break;
+                    }
+                }
+            }
+            if (targetRow != -1) break;
+            --targetCol;
+        }
+    }
+    else {
+        // if on the right then move to left
+        while (targetCol >= 0 && targetCol < cols / 2) {
+            for (int r = rows - 1; r >= 0; --r) {
+                if (ship[r][targetCol] == 0) {
+                    if (r == rows - 1 || ship[r + 1][targetCol] != 0) {
+                        targetRow = r;
+                        break;
+                    }
+                }
+            }
+            if (targetRow != -1) break;
+            ++targetCol;
+        }
+    }
+
+    // if can't find target
+    if (targetRow == -1) {
+        cout << "can't find target。" << endl;
+        return {};
+    }
+
+    // initialize node
+    Node startNode = { startRow, startCol, 0, calculateHeuristic(startRow, startCol, targetRow, targetCol), {} };
     openList.push(startNode);
 
     while (!openList.empty()) {
         Node current = openList.top();
-        openList.pop(); // check the first node
+        openList.pop();
 
-        // see if it's in the goal state
-        if ((moveToRight && current.col >= 6 && ship[current.row][current.col] == 0) || // this means the goal is move to right and it's already on the right half
-            (!moveToRight && current.col < 6 && ship[current.row][current.col] == 0)) { // this means the goal is move to left and it's already on the left half
+        // return if on goal state
+        if (current.row == targetRow && current.col == targetCol) {
             return current.path;
         }
 
-        // mark the visited point, if already marked then skip it
-        string state = to_string(current.row) + "-" + to_string(current.col); // ex:3-2
+        string state = to_string(current.row) + "-" + to_string(current.col);
         if (visited[state]) continue;
         visited[state] = true;
 
-        // check possible dir and explore
         for (auto& dir : directions) {
             int newRow = current.row + dir.first;
             int newCol = current.col + dir.second;
 
-            // check possible obstacles
-            if (newRow < 0 || newRow >= 8 || newCol < 0 || newCol >= 12) continue; // can't go beyond the ship
-            if (visited[to_string(newRow) + "-" + to_string(newCol)]) continue; // can't go back
-            if (ship[newRow][newCol] != 0) continue; // can't go through a existed node
+            if (newRow < 0 || newRow >= rows || newCol < 0 || newCol >= cols) continue;
+            if (visited[to_string(newRow) + "-" + to_string(newCol)]) continue;
+            if (ship[newRow][newCol] != 0) continue; // don't go through obstacle
 
-            // cal for new h
             int newG = current.g + 1;
-            int newH = calculateHeuristic(newRow, newCol, moveToRight);
+            int newH = calculateHeuristic(newRow, newCol, targetRow, targetCol);
 
-            // add new note to openList
             Node newNode = { newRow, newCol, newG, newH, current.path };
             newNode.path.push_back({ newRow, newCol });
             openList.push(newNode);
         }
     }
 
-    return {}; // if fail to find a route
+    return {};
 }
 
+
+
+// calculate weights on two sides
+void Balance::calculateSums() {
+    leftSum = 0;
+    rightSum = 0;
+    totalSum = 0;
+
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            if(ship[i][j] == -1){} // if it's NAN then ignore it
+            else {
+                totalSum += ship[i][j];
+                if (j < cols / 2) {
+                    leftSum += ship[i][j];
+                }
+                else {
+                    rightSum += ship[i][j];
+                }
+            }
+        }
+    }
+}
+
+void Balance::printShipWeight(){
+    cout << "weights on left half: " << leftSum << endl;
+    cout << "weights on right half: " << rightSum << endl;
+    cout << "total weight: " << totalSum << endl;
+}
+
+
+void Balance::modifyShip(int row, int col, int value) {
+    ship[row][col] = value;
+}
+
+
+int Balance::containerWeight(int row, int col) {
+    return ship[row][col];
+}
+
+
+// check if ship is balance now
+bool Balance::isBalanced() {
+    if (opitimalBalance == true) {
+        return true;
+    }
+
+    calculateSums();
+    int totalWeight = leftSum + rightSum;
+
+    if (totalWeight == 0) {
+        return true;
+    }
+
+    int tolerance = totalWeight * 0.1; // 10%
+    return abs(leftSum - rightSum) <= tolerance;
+}
+
+
+
+
+
+// print the best move route
+void Balance::printBestMove() {
+    pair<int, int> bestMove = findBestMove();
+
+    if (previousBestMove == bestMove)
+    {
+        cout << "opitimal balance achived!" << endl;
+        opitimalBalance = true;
+        return;
+    }
+    else
+    {
+        int row = bestMove.first;
+        int col = bestMove.second;
+
+        auto path1 = findShortestPath(row, col);
+        if (!path1.empty()) {
+            cout << "target position:[" << row << "][" << col << "] = " << containerWeight(row, col) << endl;
+            cout << "successful! here's the route：" << endl;
+            for (size_t i = 0; i < path1.size(); ++i) {
+                cout << "Step " << i + 1 << ": (" << path1[i].first << ", " << path1[i].second << ")" << endl;
+            }
+            modifyShip(path1[path1.size() - 1].first, path1[path1.size() - 1].second, containerWeight(row, col)); //move the weight
+            previousBestMove = { path1[path1.size() - 1].first, path1[path1.size() - 1].second };
+            modifyShip(row, col, 0); // clear origin spot
+        }
+        else {
+            cout << "failed, can't find a route" << endl;
+        }
+    }
+}
+
+bool Balance::moveObstacle(int row, int col) {
+    int targetCol = -1;
+
+    cout << "adjusting the object position above the target [" << row << "][" << col << "] before moving the target" << endl;
+
+    if (col >= cols / 2) { // right half
+        if (col < cols - 1) {
+            targetCol = col + 1; // moving one col to the right
+        }
+        else {
+            targetCol = 6; // move to col6 if at col11
+        }
+    }
+    else { // left half
+        if (col > 0) {
+            targetCol = col - 1; // moving one col to the left
+        }
+        else {
+            targetCol = 5; // move to col5 if at col0
+        }
+    }
+
+    // put it at the bottom so no floating object
+    for (int targetRow = rows - 1; targetRow >= 0; --targetRow) {
+        if (ship[targetRow][targetCol] == 0) { // empty spot
+            ship[targetRow][targetCol] = ship[row][col];
+            ship[row][col] = 0;
+            return true;
+        }
+    }
+    return false;
+}
+
+
+
+//find which is the best target to move
+pair<int, int> Balance::findBestMove() {
+    int middleNumber = totalSum / 2;
+    pair<int, int> bestMove = { -1, -1 };
+    int closestDiff = INT_MAX;
+    const int threshold = 1;
+
+    if (leftSum > rightSum) {
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols / 2; ++j) {
+                int weight = ship[i][j];
+                // skip NAN(-1)
+                if (weight != 0 && weight != -1) {
+                    // check no object above target
+                    for (int r = 0; r < i; ++r) {
+                        if (ship[r][j] > 0) {
+                            if (!moveObstacle(r, j)) {
+                                cerr << "Error: Unable to move obstacle at (" << r << ", " << j << ")." << endl;
+                                return { -1, -1 };
+                            }
+                        }
+                    }
+
+                    vector<pair<int, int>> path = findShortestPath(i, j);
+                    if (!path.empty()) {
+                        int tempRightSum = rightSum + weight;
+                        int diff = abs(tempRightSum - middleNumber);
+                        if (diff < closestDiff) {
+                            closestDiff = diff;
+                            bestMove = { i, j };
+                        }
+                        if (diff <= threshold) {
+                            return bestMove;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    else {
+        for (int i = 0; i < rows; ++i) {
+            for (int j = cols / 2; j < cols; ++j) {
+                int weight = ship[i][j];
+                // skip NAN(-1)
+                if (weight != 0 && weight != -1) {
+                    // check no object above target
+                    for (int r = 0; r < i; ++r) {
+                        if (ship[r][j] > 0) {
+                            if (!moveObstacle(r, j)) {
+                                cerr << "Error: Unable to move obstacle at (" << r << ", " << j << ").\n";
+                                return { -1, -1 };
+                            }
+                        }
+                    }
+
+                    vector<pair<int, int>> path = findShortestPath(i, j);
+                    if (!path.empty()) {
+                        int tempLeftSum = leftSum + weight;
+                        int diff = abs(tempLeftSum - middleNumber);
+                        if (diff < closestDiff) {
+                            closestDiff = diff;
+                            bestMove = { i, j };
+                        }
+                        if (diff <= threshold) {
+                            return bestMove;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return bestMove;
+}
+
+
+
+
 void Balance::printShip() {
-    cout << "current ship：" << endl;
+    cout << "current ship:" << endl;
     for (int i = 0; i < ship.size(); i++) {
         for (int j = 0; j < ship[i].size(); j++) {
-            cout << ship[i][j] << " ";
+            cout << setw(5) << ship[i][j] << " ";
         }
         cout << endl;
     }
